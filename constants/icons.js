c7cf65a47ec2ed465d4fbd976ae74bb646b4a03c
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-01-23 09:05:32 +0200

    did folder structure

--- constants/icons.js
+++ constants/icons.js
@@ -8,6 +8,9 @@ import share from "../assets/icons/share.png";
 import location from "../assets/icons/location.png";
 import chevronLeft from '../assets/icons/chevron-left.png'
 import chevronRight from '../assets/icons/chevron-right.png'
+import add_user from "../assets/icons/add_user.png"
+import rating from "../assets/icons/rating.png"
+import users from "../assets/icons/users.png"
 
 export default {
   heart,
@@ -19,5 +22,8 @@ export default {
   share,
   location,
   chevronLeft,
-  chevronRight
+  chevronRight,
+  add_user,
+  rating,
+  users
 };
